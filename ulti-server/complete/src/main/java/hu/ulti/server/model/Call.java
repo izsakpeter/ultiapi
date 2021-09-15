b8fd0ed6bc423d2e6738ecf10edd5536812c9869
Unknown <izsak.peter@tell.hu>
2021-09-15 15:47:15 +0200

    call wip

--- ulti-server/complete/src/main/java/hu/ulti/server/model/Call.java
+++ ulti-server/complete/src/main/java/hu/ulti/server/model/Call.java
@@ -4,84 +4,85 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Call {
-
-	public static final String MAKK = "makk";
-	public static final String ZOLD = "zold";
-	public static final String TOK = "tok";
-	public static final String PIROS = "piros";
-
+	
 	public static final int MAKK_COLOR_ID = 1;
 	public static final int ZOLD_COLOR_ID = 2;
 	public static final int TOK_COLOR_ID = 3;
 	public static final int PIROS_COLOR_ID = 4;
 
-	public static final int PASSZ_VALUE = 1;
-	public static final int SZAZ40_VALUE = 4;
-	public static final int ULTI_VALUE = 4;
-	public static final int BETLI_VALUE = 5;
-	public static final int DURI_VALUE = 6;
-	public static final int SZAZ20_VALUE = 8;
-	public static final int BETLI_TERITETT_VALUE = 10;
-	public static final int DURI_TERITETT_VALUE = 12;
+	private static final int PASSZ_VALUE = 1;
+	private static final int CSENDES_SZAZ_VALUE = 2;
+	private static final int CSENDES_ULTI_VALUE = 2;
+	private static final int SZAZ40_VALUE = 4;
+	private static final int ULTI_VALUE = 4;
+	private static final int BETLI_VALUE = 5;
+	private static final int DURI_VALUE = 6;
+	private static final int SZAZ20_VALUE = 8;
+	private static final int BETLI_TERITETT_VALUE = 10;
+	private static final int DURI_TERITETT_VALUE = 12;
 
 	private int id;
-	private String color;
 	private int value;
-	private int colorId;
 
-	public Call(int id, int colorId, String color, int value) {
+	public Call(int id, int value) {
 		this.id = id;
-		this.colorId = colorId;
-		this.color = color;
 		this.value = value;
 	}
 
 	public static List<Call> getAllCalls() {
 		List<Call> calls = new ArrayList<Call>();
 
-		calls.add(new Call(0, MAKK_COLOR_ID, MAKK, PASSZ_VALUE));
-		calls.add(new Call(1, MAKK_COLOR_ID, MAKK, SZAZ40_VALUE));
-		calls.add(new Call(2, MAKK_COLOR_ID, MAKK, ULTI_VALUE));
-		calls.add(new Call(3, MAKK_COLOR_ID, MAKK, BETLI_VALUE));
-		calls.add(new Call(4, MAKK_COLOR_ID, MAKK, DURI_VALUE));
-		calls.add(new Call(5, MAKK_COLOR_ID, MAKK, DURI_VALUE));
-		calls.add(new Call(6, MAKK_COLOR_ID, MAKK, SZAZ20_VALUE));
-		calls.add(new Call(7, MAKK_COLOR_ID, MAKK, BETLI_TERITETT_VALUE));
-		calls.add(new Call(8, MAKK_COLOR_ID, MAKK, DURI_TERITETT_VALUE));
-		calls.add(new Call(9, MAKK_COLOR_ID, MAKK, DURI_TERITETT_VALUE));
+		calls.add(new Call(0, PASSZ_VALUE * MAKK_COLOR_ID));
+		calls.add(new Call(1, CSENDES_SZAZ_VALUE * MAKK_COLOR_ID));
+		calls.add(new Call(2, CSENDES_ULTI_VALUE * MAKK_COLOR_ID));
+		calls.add(new Call(3, SZAZ40_VALUE * MAKK_COLOR_ID));
+		calls.add(new Call(4, ULTI_VALUE * MAKK_COLOR_ID));
+		calls.add(new Call(5, BETLI_VALUE * MAKK_COLOR_ID));
+		calls.add(new Call(6, DURI_VALUE * MAKK_COLOR_ID));
+		calls.add(new Call(7, DURI_VALUE * MAKK_COLOR_ID));
+		calls.add(new Call(8, SZAZ20_VALUE * MAKK_COLOR_ID));
+		calls.add(new Call(9, BETLI_TERITETT_VALUE * MAKK_COLOR_ID));
+		calls.add(new Call(10, DURI_TERITETT_VALUE * MAKK_COLOR_ID));
+		calls.add(new Call(11, DURI_TERITETT_VALUE * MAKK_COLOR_ID));
 
-		calls.add(new Call(10, ZOLD_COLOR_ID, ZOLD, PASSZ_VALUE * ZOLD_COLOR_ID));
-		calls.add(new Call(11, ZOLD_COLOR_ID, ZOLD, SZAZ40_VALUE * ZOLD_COLOR_ID));
-		calls.add(new Call(12, ZOLD_COLOR_ID, ZOLD, ULTI_VALUE * ZOLD_COLOR_ID));
-		calls.add(new Call(13, ZOLD_COLOR_ID, ZOLD, BETLI_VALUE * ZOLD_COLOR_ID));
-		calls.add(new Call(14, ZOLD_COLOR_ID, ZOLD, DURI_VALUE * ZOLD_COLOR_ID));
-		calls.add(new Call(15, ZOLD_COLOR_ID, ZOLD, DURI_VALUE * ZOLD_COLOR_ID));
-		calls.add(new Call(16, ZOLD_COLOR_ID, ZOLD, SZAZ20_VALUE * ZOLD_COLOR_ID));
-		calls.add(new Call(17, ZOLD_COLOR_ID, ZOLD, BETLI_TERITETT_VALUE * ZOLD_COLOR_ID));
-		calls.add(new Call(18, ZOLD_COLOR_ID, ZOLD, DURI_TERITETT_VALUE * ZOLD_COLOR_ID));
-		calls.add(new Call(19, ZOLD_COLOR_ID, ZOLD, DURI_TERITETT_VALUE * ZOLD_COLOR_ID));
+		calls.add(new Call(12, PASSZ_VALUE * ZOLD_COLOR_ID));
+		calls.add(new Call(13, CSENDES_SZAZ_VALUE * MAKK_COLOR_ID));
+		calls.add(new Call(14, CSENDES_ULTI_VALUE * MAKK_COLOR_ID));
+		calls.add(new Call(15, SZAZ40_VALUE * ZOLD_COLOR_ID));
+		calls.add(new Call(16, ULTI_VALUE * ZOLD_COLOR_ID));
+		calls.add(new Call(17, BETLI_VALUE * ZOLD_COLOR_ID));
+		calls.add(new Call(18, DURI_VALUE * ZOLD_COLOR_ID));
+		calls.add(new Call(19, DURI_VALUE * ZOLD_COLOR_ID));
+		calls.add(new Call(20, SZAZ20_VALUE * ZOLD_COLOR_ID));
+		calls.add(new Call(21, BETLI_TERITETT_VALUE * ZOLD_COLOR_ID));
+		calls.add(new Call(22, DURI_TERITETT_VALUE * ZOLD_COLOR_ID));
+		calls.add(new Call(23, DURI_TERITETT_VALUE * ZOLD_COLOR_ID));
 
-		calls.add(new Call(20, TOK_COLOR_ID, TOK, PASSZ_VALUE * TOK_COLOR_ID));
-		calls.add(new Call(21, TOK_COLOR_ID, TOK, SZAZ40_VALUE * TOK_COLOR_ID));
-		calls.add(new Call(22, TOK_COLOR_ID, TOK, ULTI_VALUE * TOK_COLOR_ID));
-		calls.add(new Call(23, TOK_COLOR_ID, TOK, BETLI_VALUE * TOK_COLOR_ID));
-		calls.add(new Call(24, TOK_COLOR_ID, TOK, DURI_VALUE * TOK_COLOR_ID));
-		calls.add(new Call(25, TOK_COLOR_ID, TOK, DURI_VALUE * TOK_COLOR_ID));
-		calls.add(new Call(26, TOK_COLOR_ID, TOK, SZAZ20_VALUE * TOK_COLOR_ID));
-		calls.add(new Call(27, TOK_COLOR_ID, TOK, BETLI_TERITETT_VALUE * TOK_COLOR_ID));
-		calls.add(new Call(28, TOK_COLOR_ID, TOK, DURI_TERITETT_VALUE * TOK_COLOR_ID));
-		calls.add(new Call(29, TOK_COLOR_ID, TOK, DURI_TERITETT_VALUE * TOK_COLOR_ID));
+		calls.add(new Call(20, PASSZ_VALUE * TOK_COLOR_ID));
+		calls.add(new Call(1, CSENDES_SZAZ_VALUE * MAKK_COLOR_ID));
+		calls.add(new Call(2, CSENDES_ULTI_VALUE * MAKK_COLOR_ID));
+		calls.add(new Call(21, SZAZ40_VALUE * TOK_COLOR_ID));
+		calls.add(new Call(22, ULTI_VALUE * TOK_COLOR_ID));
+		calls.add(new Call(23, BETLI_VALUE * TOK_COLOR_ID));
+		calls.add(new Call(24, DURI_VALUE * TOK_COLOR_ID));
+		calls.add(new Call(25, DURI_VALUE * TOK_COLOR_ID));
+		calls.add(new Call(26, SZAZ20_VALUE * TOK_COLOR_ID));
+		calls.add(new Call(27, BETLI_TERITETT_VALUE * TOK_COLOR_ID));
+		calls.add(new Call(28, DURI_TERITETT_VALUE * TOK_COLOR_ID));
+		calls.add(new Call(29, DURI_TERITETT_VALUE * TOK_COLOR_ID));
 
-		calls.add(new Call(30, PIROS_COLOR_ID, PIROS, PASSZ_VALUE * PIROS_COLOR_ID));
-		calls.add(new Call(31, PIROS_COLOR_ID, PIROS, SZAZ40_VALUE * PIROS_COLOR_ID));
-		calls.add(new Call(32, PIROS_COLOR_ID, PIROS, ULTI_VALUE * PIROS_COLOR_ID));
-		calls.add(new Call(33, PIROS_COLOR_ID, PIROS, BETLI_VALUE * PIROS_COLOR_ID));
-		calls.add(new Call(34, PIROS_COLOR_ID, PIROS, DURI_VALUE * PIROS_COLOR_ID));
-		calls.add(new Call(35, PIROS_COLOR_ID, PIROS, DURI_VALUE * PIROS_COLOR_ID));
-		calls.add(new Call(36, PIROS_COLOR_ID, PIROS, SZAZ20_VALUE * PIROS_COLOR_ID));
-		calls.add(new Call(37, PIROS_COLOR_ID, PIROS, BETLI_TERITETT_VALUE * PIROS_COLOR_ID));
-		calls.add(new Call(38, PIROS_COLOR_ID, PIROS, DURI_TERITETT_VALUE * PIROS_COLOR_ID));
-		calls.add(new Call(39, PIROS_COLOR_ID, PIROS, DURI_TERITETT_VALUE * PIROS_COLOR_ID));
+		calls.add(new Call(30, PASSZ_VALUE * PIROS_COLOR_ID));
+		calls.add(new Call(1, CSENDES_SZAZ_VALUE * MAKK_COLOR_ID));
+		calls.add(new Call(2, CSENDES_ULTI_VALUE * MAKK_COLOR_ID));
+		calls.add(new Call(31, SZAZ40_VALUE * PIROS_COLOR_ID));
+		calls.add(new Call(32, ULTI_VALUE * PIROS_COLOR_ID));
+		calls.add(new Call(33, BETLI_VALUE * PIROS_COLOR_ID));
+		calls.add(new Call(34, DURI_VALUE * PIROS_COLOR_ID));
+		calls.add(new Call(35, DURI_VALUE * PIROS_COLOR_ID));
+		calls.add(new Call(36, SZAZ20_VALUE * PIROS_COLOR_ID));
+		calls.add(new Call(37, BETLI_TERITETT_VALUE * PIROS_COLOR_ID));
+		calls.add(new Call(38, DURI_TERITETT_VALUE * PIROS_COLOR_ID));
+		calls.add(new Call(39, DURI_TERITETT_VALUE * PIROS_COLOR_ID));
 
 		return calls;
 	}
@@ -94,28 +95,12 @@ public class Call {
 		this.id = id;
 	}
 
-	public String getColor() {
-		return color;
-	}
-
-	public void setColor(String color) {
-		this.color = color;
-	}
-
 	public int getValue() {
 		return value;
 	}
 
 	public void setValue(int value) {
 		this.value = value;
-	}
-
-	public int getColorId() {
-		return colorId;
-	}
-
-	public void setColorId(int colorId) {
-		this.colorId = colorId;
 	}
 	
 	public static boolean isTeritett(List<Integer> previousCall) {
